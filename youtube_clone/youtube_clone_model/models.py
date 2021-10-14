5830b42a04351ca133aa2cee1f512c7e0aa07c68
TGWolf <60626007+sfodd1@users.noreply.github.com>
2021-10-14 10:37:25 -0400

    add likes and dislikes to model

--- youtube_clone/youtube_clone_model/models.py
+++ youtube_clone/youtube_clone_model/models.py
@@ -5,6 +5,8 @@ class Comment(models.Model):
     username = models.CharField(max_length=50)
     body = models.CharField(max_length=150)
     videoId = models.CharField(max_length=150)
+    likes = models.IntegerField(default=0,null=False)
+    dislikes = models.IntegerField(default=0,null=False) 
 
 class Reply(models.Model):
     body = models.CharField(max_length=150)  
